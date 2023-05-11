109c57e89663af2df3f3e47a922106b1bbe88217
ardbl3idd <dawid.dudek@codema.pl>
2023-05-11 12:45:25 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "animate.css";
-import App from "./App.vue";
-import "./assets/main.css";
+import App from "App.vue";
+import "/assets/main.css";
 import "animate.css";
 createApp(App).mount("#app");
 // function previewFile(input) {
